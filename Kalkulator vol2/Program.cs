01dd5e9a89dcb5fdcf6b64aa835510d97129eb92
bigpaulus <paulsarnecki@gmail.com>
2020-03-19 12:10:58 +0100

    Dodaj pliki projektów.

---
+++ Kalkulator vol2/Program.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator_vol2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            decimal plus;
+            decimal minus;
+            decimal mnozenie;
+            decimal dzielenie;
+
+
+            decimal result;
+            
+            poczatek:
+            try
+            {
+                Console.Write("Wpisz liczbę:  ");
+                decimal a = Convert.ToDecimal(Console.ReadLine());
+
+
+                Console.Write("Wybierz ( +, -, *, / ):  ");
+                string z1 = Console.ReadLine();
+
+
+                Console.Write("Wpisz kolejną liczbę:  ");
+                decimal b = Convert.ToDecimal(Console.ReadLine());
+
+
+                Console.Write("Wybierz ( +, -, *, / ) lub znak ( = ):  ");
+                string z2 = Console.ReadLine();
+            
+            
+
+
+
+
+
+
+            if (z1 == "+" && z2 == "=")
+            {
+
+                plus = a + b;
+
+                Console.WriteLine("Wynik to:  " + plus);
+                Console.ReadKey();
+                Console.Clear();
+                goto poczatek;
+            }
+
+
+
+            if (z1 == "-" && z2 == "=")
+            {
+                minus = a - b;
+
+                Console.WriteLine("Wynik to:  " + minus);
+                Console.ReadKey();
+                Console.Clear();
+                goto poczatek;
+            }
+
+
+
+            if (z1 == "*" && z2 == "=")
+            {
+                mnozenie = a * b;
+
+                Console.WriteLine("Wynik to:  " + mnozenie);
+                Console.ReadKey();
+                Console.Clear();
+                goto poczatek;
+            }
+
+
+
+            if (z1 == "/" && z2 == "=")
+            {
+                dzielenie = a / b;
+
+                Console.WriteLine("Wynik to:  " + dzielenie);
+                Console.ReadKey();
+                Console.Clear();
+                goto poczatek;
+
+            }
+
+
+
+            else
+            {
+
+
+
+                Console.Write("Wpisz ostatnią liczbę:  ");
+                decimal c = Convert.ToDecimal(Console.ReadLine());
+
+               
+
+                //działanie po dodawaniu
+
+                if (z1 == "+" && z2 == "+" )
+                {
+
+                    result = a + b + c;
+
+                    Console.WriteLine("Wynik to:  " + result);
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto poczatek;
+                }
+
+
+
+                if (z1 == "+" && z2 == "-" )
+                {
+                    result = a + b - c;
+
+                    Console.WriteLine("Wynik to:  " + result);
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto poczatek;
+                }
+
+
+
+                if (z1 == "+" && z2 == "*" )
+                {
+                    result = a + b * c;
+
+                    Console.WriteLine("Wynik to:  " + result);
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto poczatek;
+                }
+
+
+
+                if (z1 == "+" && z2 == "/" )
+                {
+                    result = a + b / c;
+
+                    Console.WriteLine("Wynik to:  " + result);
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto poczatek;
+
+                }
+
+
+                //działanie po odejmowaniu
+
+
+                if (z1 == "-" && z2 == "+" )
+                {
+
+                    result = a - b + c;
+
+                    Console.WriteLine("Wynik to:  " + result);
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto poczatek;
+                }
+
+
+
+                if (z1 == "-" && z2 == "-" )
+                {
+                    result = a - b - c;
+
+                    Console.WriteLine("Wynik to:  " + result);
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto poczatek;
+                }
+
+
+
+                if (z1 == "-" && z2 == "*" )
+                {
+                    result = a - b * c;
+
+                    Console.WriteLine("Wynik to:  " + result);
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto poczatek;
+                }
+
+
+
+                if (z1 == "-" && z2 == "/" )
+                {
+                    result = a - b / c;
+
+                    Console.WriteLine("Wynik to:  " + result);
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto poczatek;
+
+                }
+
+
+
+
+                //działanie po mnożeniu
+
+                if (z1 == "*" && z2 == "+" )
+                {
+
+                    result = a * b + c;
+
+                    Console.WriteLine("Wynik to:  " + result);
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto poczatek;
+                }
+
+
+
+                if (z1 == "*" && z2 == "-" )
+                {
+                    result = a * b - c;
+
+                    Console.WriteLine("Wynik to:  " + result);
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto poczatek;
+                }
+
+
+
+                if (z1 == "*" && z2 == "*" )
+                {
+                    result = a * b * c;
+
+                    Console.WriteLine("Wynik to:  " + result);
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto poczatek;
+                }
+
+
+
+                if (z1 == "*" && z2 == "/" )
+                {
+                    result = a * b / c;
+
+                    Console.WriteLine("Wynik to:  " + result);
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto poczatek;
+                }
+
+
+
+
+                //działanie po dzieleniu
+
+                if (z1 == "/" && z2 == "+" )
+                {
+
+                    result = a / b + c;
+
+                    Console.WriteLine("Wynik to:  " + result);
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto poczatek;
+                }
+
+
+
+                if (z1 == "/" && z2 == "-")
+                {
+                    result = a / b - c;
+
+                    Console.WriteLine("Wynik to:  " + result);
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto poczatek;
+                }
+
+
+
+                if (z1 == "/" && z2 == "*" )
+                {
+                    result = a / b * c;
+
+                    Console.WriteLine("Wynik to:  " + result);
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto poczatek;
+                }
+
+
+
+                if (z1 == "/" && z2 == "/" )
+                {
+                    result = a / b / c;
+
+                    Console.WriteLine("Wynik to:  " +result);
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto poczatek;
+
+                }
+
+
+            }
+
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Wprowadź poprawny znak"); ; ;
+                Console.ReadKey();
+                Console.Clear();
+                goto poczatek;
+            }
+
+
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
